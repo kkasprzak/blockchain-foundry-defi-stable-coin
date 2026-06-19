6a55b8d87b1c0079c36251455fdab4a1ab0d8e3d
Karol Kasprzak <karol.kasprzak@boldare.com>
2026-06-19 09:25:08 +0200

    feat: burn and mint tokens

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -6,9 +6,39 @@ import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensio
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    error DSC__MustBeMoreThanZero();
+    error DSC__BurnAmountExceedsBalance();
+    error DSC__RecipientAddressIncorect();
+
     constructor(
         address initialOwner
     ) ERC20("DecentralizedStableCoin", "DSC") Ownable(initialOwner) {}
 
-    function burn(uint256 value) public override onlyOwner {}
+    function burn(uint256 amount) public override onlyOwner {
+        if (amount <= 0) {
+            revert DSC__MustBeMoreThanZero();
+        }
+
+        uint256 balance = balanceOf(msg.sender);
+        if (amount > balance) {
+            revert DSC__BurnAmountExceedsBalance();
+        }
+        super.burn(amount);
+    }
+
+    function mint(
+        address to,
+        uint256 amount
+    ) external onlyOwner returns (bool) {
+        if (address(0) == to) {
+            revert DSC__RecipientAddressIncorect();
+        }
+
+        if (amount <= 0) {
+            revert DSC__MustBeMoreThanZero();
+        }
+        _mint(to, amount);
+
+        return false;
+    }
 }
---
+++ test/DecentralizedStableCoinTest.sol
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.21;
+
+import {Test} from "forge-std/Test.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DecentralizedStableCoinTest is Test {
+    DecentralizedStableCoin dsc;
+    address dscOwner;
+
+    function setUp() public {
+        dscOwner = makeAddr("dscOwner");
+        dsc = new DecentralizedStableCoin(dscOwner);
+    }
+
+    function testOnlyOwnerCanBurnTokens() public {
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                Ownable.OwnableUnauthorizedAccount.selector,
+                address(this)
+            )
+        );
+        dsc.burn(10);
+    }
+
+    function testBurnThrowsErrorWhenAmountIsLessOrEqualToZero() public {
+        vm.prank(dscOwner);
+
+        vm.expectRevert(
+            DecentralizedStableCoin.DSC__MustBeMoreThanZero.selector
+        );
+        dsc.burn(0);
+    }
+
+    function testBurnThrowsErrorWhenUserDoesNotHaveEnoughtBalanceOnHisAccount()
+        public
+    {
+        vm.prank(dscOwner);
+
+        vm.expectRevert(
+            DecentralizedStableCoin.DSC__BurnAmountExceedsBalance.selector
+        );
+        dsc.burn(10);
+    }
+
+    function testBurnReducesBalanceOfTokenOnTheUserAccount() public {
+        uint256 amountToBurn = 10 ether;
+        uint256 initialBalance = 100 ether;
+
+        deal(address(dsc), dscOwner, initialBalance);
+
+        vm.prank(dscOwner);
+        dsc.burn(amountToBurn);
+
+        assertEq(dsc.balanceOf(dscOwner), initialBalance - amountToBurn);
+    }
+
+    function testOnlyOwnerCanMintTokens() public {
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                Ownable.OwnableUnauthorizedAccount.selector,
+                address(this)
+            )
+        );
+        dsc.mint(makeAddr("user"), 10);
+    }
+
+    function testMintThrowsErrorWhenRecipientAddressIsIncorrect() public {
+        vm.prank(dscOwner);
+
+        vm.expectRevert(
+            DecentralizedStableCoin.DSC__RecipientAddressIncorect.selector
+        );
+        dsc.mint(address(0), 10);
+    }
+
+    function testMintThrowsErrorWhenAmountIsLessOrEqualToZero() public {
+        vm.prank(dscOwner);
+
+        vm.expectRevert(
+            DecentralizedStableCoin.DSC__MustBeMoreThanZero.selector
+        );
+        dsc.mint(makeAddr("user"), 0);
+    }
+
+    function testMintIncreasesBalanceOfTokensOnUserAccount() public {
+        uint256 amountToMint = 10 ether;
+        uint256 initialBalance = 100 ether;
+        address userAddress = makeAddr("user");
+
+        deal(address(dsc), userAddress, initialBalance);
+
+        vm.prank(dscOwner);
+        dsc.mint(userAddress, amountToMint);
+
+        assertEq(dsc.balanceOf(userAddress), initialBalance + amountToMint);
+    }
+}
